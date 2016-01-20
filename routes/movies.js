8076176d71414b67fcdb6115cc958f6446c4fa21
June Shaw <june.shaw@me.com>
2016-01-19 17:14:48 -0700

    initial commit

--- routes/movies.js
+++ routes/movies.js
@@ -7,4 +7,55 @@ router.get('/', function (req, res, next) {
   res.render('movies/index');
 })
 
+router.get('/new', function (req, res, next) {
+  res.render('movies/new');
+})
+
+router.post('/', function(req, res, next) {
+  res.redirect('/movies');
+})
+
+router.get('/:id', function(req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:id/edit', function(req, res, next) {
+  res.render('movies/edit');
+})
+
+router.post('/:id', function(req, res, next) {
+  res.redirect('/movies');
+})
+
+router.post('/:id/delete', function(req, res, next) {
+  res.redirect('/movies');
+})
+
+router.get('/:movie_id/reviews', function(req, res, next) {
+  res.render('movies/reviews/show');
+})
+
+router.get('/:movie_id/reviews/new', function(req, res, next) {
+  res.render('movies/reviews/new');
+})
+
+router.post('/:movie_id/reviews', function(req, res, next) {
+  res.redirect('/movies/:movie_id');
+})
+
+router.post('/:movie_id/reviews/:id', function(req, res, next) {
+  res.redirect('/movies/:movie_id/reviews')
+})
+
+router.get('/:movie_id/reviews/:id/edit', function(req, res, next) {
+  res.render('movies/reviews/edit');
+})
+
+router.get('/:movie_id/reviews/:id', function(req, res, next) {
+  res.render('movies/reviews/show');
+})
+
+router.post('/:movie_id/reviews/:id/delete', function(req, res, next) {
+  res.redirect('/movies/show')
+})
 module.exports = router;
